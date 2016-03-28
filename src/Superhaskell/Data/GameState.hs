00645ecc8b241d6915569c1df9ff594408a54133
Simon Strassl <simon.strassl@gmail.com>
2016-03-28 23:16:43 +0200

    Add dummy world generation

--- src/Superhaskell/Data/GameState.hs
+++ src/Superhaskell/Data/GameState.hs
@@ -9,10 +9,12 @@ module Superhaskell.Data.GameState (
 import           Control.DeepSeq
 import           GHC.Generics
 import           Superhaskell.Data.Entity
+import           Linear.V2 (V2(..))
 
 data GameState = GameState { entities :: [Entity]
                            , running  :: Bool
                            , gsGenState :: GenState
+                           , gsViewPort :: V2 Float
                            }
                deriving (Show, Generic, NFData)
 
@@ -24,7 +26,9 @@ data GenState = GenState { genBound :: Float
 initialGameState :: GameState
 initialGameState = GameState { running = True
                              , entities = []
-                             , gsGenState = initialGenState }
+                             , gsGenState = initialGenState
+                             , gsViewPort = V2 16 9
+                             }
 
 -- Stores information that the generation component needs across iterations
 -- Such as up to where it already generated the world
--- src/Superhaskell/Generation.hs
+++ src/Superhaskell/Generation.hs
@@ -16,37 +16,33 @@ import Superhaskell.Data.GameState
 import Superhaskell.Data.Entity
 import Superhaskell.Math
 
-type VPBound = Float
-
 updateWorld :: RandomGen g => GameState -> Rand g GameState
-updateWorld gs@GameState{entities = es} = do
-  generated <- generate (gsGenState gs)
-  let nBound = maximum $ map ((^._x) . rightBottom . eBox) generated
-  let pruned = prune 0 es
+updateWorld gs@GameState{entities = es, gsViewPort = vp} = do
+  generated <- generate vp (gsGenState gs)
+  let nBound = maximum $ genBound (gsGenState gs):map ((^._x) . rightBottom . eBox) generated
+  let pruned = prune 0 es -- TODO needs an actual left bound
   let nGenState = (gsGenState gs) { genBound = nBound }
-  return gs{ entities = pruned ++ generated
+  let nes = pruned ++ generated
+  return gs{ entities = nes
            , gsGenState = nGenState }
 
-generate :: RandomGen g => GenState -> Rand g [Entity]
-generate _ = return mockScenery
+prune :: Float -> [Entity] -> [Entity]
+prune vpleft = filter (not . isLeftOfViewport vpleft)
 
-prune :: VPBound -> [Entity] -> [Entity]
-prune vpleft = filter (isLeftOfViewport vpleft)
+-- TODO This whole function needs a real implementation
+generate :: RandomGen g => V2 Float -> GenState -> Rand g [Entity]
+generate _vp@(V2 w h) _gs@GenState{genBound = bound}
+  | bound >= w = return []
+  | otherwise = do
+    pos_x <- getRandomR (bound, w) -- TODO Actually not 100% correct because it ignores width - recheck this
+    pos_y <- getRandomR (1, h-1)
+    let p = platform (V3 pos_x pos_y 0) 3
+    let c = silverCoin (V3 (pos_x+1.5) (pos_y - 1.5) 1)
+    return [p, c]
 
-mockScenery :: [Entity]
-mockScenery = [ platform (V3 0 0 0) 3
-              , platform (V3 4 0.5 0) 6
-              , silverCoin (V3 6.5 6.5 0)
-              , silverCoin (V3 7 7 1)
-              , silverCoin (V3 5.5 5.5 (-2))
-              , silverCoin (V3 7.5 7.5 2)
-              , silverCoin (V3 6 6 (-1))
-              , silverCoin (V3 5 5 (-3))
-              ]
-
-isLeftOfViewport :: VPBound -> Entity -> Bool
+isLeftOfViewport :: Float -> Entity -> Bool
 isLeftOfViewport leftBound _e@Entity{eBox = box} = boxRight <= leftBound
-  where boxRight = (rightBottom box) ^._x
+  where boxRight = rightBottom box ^._x
 
 platform :: V3 Float -> Float -> Entity
 platform pos length = Entity { eBox = Box pos (V2 length 0.75)
