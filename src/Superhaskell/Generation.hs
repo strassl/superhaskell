2fc53993fc2082832a5235acbffde937b84c9973
Simon Strassl <simon.strassl@gmail.com>
2016-03-29 00:31:25 +0200

    Add basic platform generation algorithm

--- src/Superhaskell/Generation.hs
+++ src/Superhaskell/Generation.hs
@@ -29,16 +29,29 @@ updateWorld gs@GameState{entities = es, gsViewPort = vp} = do
 prune :: Float -> [Entity] -> [Entity]
 prune vpleft = filter (not . isLeftOfViewport vpleft)
 
--- TODO This whole function needs a real implementation
+-- TODO Use dynamic viewport here
+-- TODO correct height
+-- We partition the world (horizontally) into partitionWidth wide sections (at least 1)
+-- In each partition we generate a single platform
 generate :: RandomGen g => V2 Float -> GenState -> Rand g [Entity]
 generate _vp@(V2 w h) _gs@GenState{genBound = bound}
   | bound >= w = return []
   | otherwise = do
-    pos_x <- getRandomR (bound, w) -- TODO Actually not 100% correct because it ignores width - recheck this
-    pos_y <- getRandomR (1, h-1)
+    let parts = partition bound w
+    sequence $ map (generatePlatform (0, h)) parts
+
+partition :: Float -> Float -> [(Float, Float)]
+partition l r = zip parts (tail' parts)
+  where w = r - l
+        pc = floor(w / partitionWidth)
+        parts = scanl (+) l (replicate pc partitionWidth)
+
+generatePlatform :: RandomGen g => (Float, Float) -> (Float, Float) -> Rand g Entity
+generatePlatform (b, t) (l, r)= do
+    pos_x <- getRandomR (l, r) -- TODO Actually not 100% correct because it ignores width - recheck this
+    pos_y <- getRandomR (b, t)
     let p = platform (V3 pos_x pos_y 0) 3
-    let c = silverCoin (V3 (pos_x+1.5) (pos_y - 1.5) 1)
-    return [p, c]
+    return p
 
 isLeftOfViewport :: Float -> Entity -> Bool
 isLeftOfViewport leftBound _e@Entity{eBox = box} = boxRight <= leftBound
@@ -55,3 +68,10 @@ silverCoin pos = Entity { eBox = Box pos (V2 1 1)
                         , eStyle = BoxStyle "silver_1"
                         , eBehavior = NoopBehavior
                         , eCollisionGroup = NilCGroup}
+
+partitionWidth :: Float
+partitionWidth = 4
+
+tail' :: [a] -> [a]
+tail' [] = []
+tail' (x:xs) = xs
