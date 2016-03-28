839d4dae5c5f159dfe0f14e4cbac840b5b24296b
Simon Strassl <simon.strassl@gmail.com>
2016-03-28 21:41:05 +0200

    Add corner functions for box

--- src/Superhaskell/Math.hs
+++ src/Superhaskell/Math.hs
@@ -1,7 +1,12 @@
 {-# LANGUAGE DeriveAnyClass #-}
 {-# LANGUAGE DeriveGeneric  #-}
-module Superhaskell.Math (Box(..)) where
+module Superhaskell.Math (
+    Box(..)
+  , leftTop
+  , rightBottom
+) where
 
+import           Control.Lens
 import           Control.DeepSeq
 import           GHC.Generics
 import           Linear
@@ -9,3 +14,20 @@ import           Linear
 data Box = Box { boxAnchor :: V3 Float
                , boxSize   :: V2 Float }
          deriving (Show, Generic, NFData)
+
+leftTop :: Box -> V2 Float
+leftTop _b@Box{boxAnchor = anchor} = withoutZ anchor
+
+leftBottom :: Box -> V2 Float
+leftBottom _b@Box{boxAnchor = anchor, boxSize = size} = over _y addHeight (withoutZ anchor)
+  where addHeight = (+ (size ^._y))
+
+rightBottom :: Box -> V2 Float
+rightBottom _b@Box{boxAnchor = anchor, boxSize = size} = (withoutZ anchor) + size
+
+rightTop :: Box -> V2 Float
+rightTop _b@Box{boxAnchor = anchor, boxSize = size} = over _x addWidth (withoutZ anchor)
+  where addWidth = (+ (size ^._x))
+
+withoutZ :: V3 t -> V2 t
+withoutZ = (^._xy)
