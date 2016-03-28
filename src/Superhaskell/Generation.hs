fe776d91019792dd7ea8d7077ef3e6da3480f987
Simon Strassl <simon.strassl@gmail.com>
2016-03-28 22:05:17 +0200

    Make platform/coin constructors more specific

--- src/Superhaskell/Generation.hs
+++ src/Superhaskell/Generation.hs
@@ -31,28 +31,28 @@ prune :: VPBound -> [Entity] -> [Entity]
 prune vpleft = filter (isLeftOfViewport vpleft)
 
 mockScenery :: [Entity]
-mockScenery = [platform {eBox = Box (V3 0 0 0) (V2 3 0.75) }
-              , platform {eBox = Box (V3 4 0.5 0) (V2 6 0.75) }
-              , silverCoin {eBox = Box (V3 6.5 6.5 0) (V2 1 1) }
-              , silverCoin {eBox = Box (V3 7 7 1) (V2 1 1) }
-              , silverCoin {eBox = Box (V3 5.5 5.5 (-2)) (V2 1 1) }
-              , silverCoin {eBox = Box (V3 7.5 7.5 2) (V2 1 1) }
-              , silverCoin {eBox = Box (V3 6 6 (-1)) (V2 1 1) }
-              , silverCoin {eBox = Box (V3 5 5 (-3)) (V2 1 1) }
+mockScenery = [ platform (V3 0 0 0) 3
+              , platform (V3 4 0.5 0) 6
+              , silverCoin (V3 6.5 6.5 0)
+              , silverCoin (V3 7 7 1)
+              , silverCoin (V3 5.5 5.5 (-2))
+              , silverCoin (V3 7.5 7.5 2)
+              , silverCoin (V3 6 6 (-1))
+              , silverCoin (V3 5 5 (-3))
               ]
 
 isLeftOfViewport :: VPBound -> Entity -> Bool
 isLeftOfViewport leftBound _e@Entity{eBox = box} = boxRight <= leftBound
   where boxRight = (rightBottom box) ^._x
 
-platform :: Entity
-platform = Entity {eBox = Box (V3 0 0 0) (V2 0 0)
-                  , eStyle = BoxStyle "ground_stone"
-                  , eBehavior = NoopBehavior
-                  , eCollisionGroup = SceneryCGroup}
+platform :: V3 Float -> Float -> Entity
+platform pos length = Entity { eBox = Box pos (V2 length 0.75)
+                             , eStyle = BoxStyle "ground_stone"
+                             , eBehavior = NoopBehavior
+                             , eCollisionGroup = SceneryCGroup}
 
-silverCoin :: Entity
-silverCoin = Entity {eBox = Box (V3 0 0 0) (V2 0 0)
-                    , eStyle = BoxStyle "silver_1"
-                    , eBehavior = NoopBehavior
-                    , eCollisionGroup = NilCGroup}
+silverCoin :: V3 Float -> Entity
+silverCoin pos = Entity { eBox = Box pos (V2 1 1)
+                        , eStyle = BoxStyle "silver_1"
+                        , eBehavior = NoopBehavior
+                        , eCollisionGroup = NilCGroup}
